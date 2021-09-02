77259fe8ded2253150665f9fc114b43c857a1027
tabatabb7 <yoosallyh@gmail.com>
2021-09-02 17:49:59 -0400

    feat: styled links and added dummy row

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./Home.css";
-import logo from "./roseapothlogo.jpeg";
+import logo from "./roseapothlogo.png";
+import basket from "./basket.png";
 
 function Home() {
   return (
     <div className="logo">
-      <img src={logo} alt="Rose Apothecary" id="logo" />
+      <a href="/" className="logoimg">
+        <img src={logo} alt="Rose Apothecary" />
+      </a>
       <div className="links">
-        <a href="/shop" className="links">
-          SHOP*
+        <a href="/shop" className="link">
+          SHOP
         </a>
 
-        <a href="/account" className="links">
-          *LOGIN/SIGNUP
+        <a href="/account" className="link">
+          LOGIN/SIGNUP
+        </a>
+        <a href="basket" className="link">
+          <img src={basket} alt="basket"></img>
         </a>
       </div>
     </div>
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,7 +1,33 @@
 import React from "react";
-
+import "./Shop.css";
+import candle from "./candle.png";
+import perfume from "./perfume.png";
+import bodymilk from "./bodymilk.png";
 function Shop() {
-  return "SHOP";
+  return (
+    <div id="productlisting">
+      <div class="row">
+        <div class="column">
+          <a href="/products/1">
+            <img src={candle} alt="scentedcandle"></img>
+          </a>
+          <p>Scented Candle</p>
+        </div>
+        <div class="column">
+          <a href="/products/2">
+            <img src={perfume} alt="perfume"></img>
+          </a>
+          <p>perfume</p>
+        </div>
+        <div class="column">
+          <a href="/products/3">
+            <img src={bodymilk} alt="bodymilk"></img>
+          </a>
+          <p>bodymilk</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Shop;
