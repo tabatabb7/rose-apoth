5118169b4c9d56c9c794546796d60c6699ac4a99
tabatabb7 <yoosallyh@gmail.com>
2021-09-28 14:23:28 -0400

    fix: fixed logo image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,22 @@
 import React from "react";
 import "./Home.css";
-import logo from "../images/roseapothlogo.png";
-import basket from "../images/basket.png";
-import account from "../images/account.png";
-import shop from "../images/shop.png";
 
 function Home() {
   return (
     <div className="logo">
       <a href="/" className="logoimg">
-        <img src={logo} alt="Rose Apothecary" />
+        <img src="/images/roseapothlogo.png" alt="Rose Apothecary" />
       </a>
       <div className="links">
         <a href="/shop" className="link">
-          <img src={shop} alt="shop"></img>
+          <img src="/images/shop.png" alt="shop"></img>
         </a>
 
         <a href="/account" className="link">
-          <img src={account} alt="account"></img>
+          <img src="/images/account.png" alt="account"></img>
         </a>
         <a href="/basket" className="link">
-          <img src={basket} alt="basket"></img>
+          <img src="/images/basket.png" alt="basket"></img>
         </a>
       </div>
     </div>
